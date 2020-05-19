56fa1721417c443381cebc1e3199d4fcaa60a2b1
cwalla <4538666+cwalla@users.noreply.github.com>
2020-05-19 10:37:56 -0700

    - Each session is now added to a map indexed by channel ID to allow for multiple groups to use the bot simultaneously  - The !self command has been deprecated as it's made redundant by !settraitor  - Improved error handling by adding try/catch blocks to ensure the bot doesn't crash if a command is send via DM  - Re-factored poorly named viables / functions

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const auth = require('./token.json');
 const token = auth.token;
 
 const treachery = require('./scenarios.json');
-var traitor = '';
+let allSessions = new Map();
 
 //IMPORTANT: Comment out this line before Heroku deployment:
 client.login(token);
@@ -16,73 +16,98 @@ client.on('ready', () => {
  });
 
 //Event listener for command messages
- client.on('message', message => {
+client.on('message', message => {
    if (message.content === '!ping') {
      message.channel.send('Pong!');
    }
 
    //Set a random player as the traitor
    if (message.content === '!rolltraitor'||message.content === '!roll') {
-     if (!message.member.voice.channel) {
-       return message.reply('You aren\'t in any voice channels!' )
-     }
-     const crew = init(message);
-     traitor = crew[getRandomInt(crew.length)];
-     message.reply('There\'s a traitor in your midst...');
-     traitor.send("You are the traitor.");
+     try {
+       if (!message.member.voice.channel) {
+         return message.reply('You aren\'t in any voice channels!' )
+       }
+       const chanID = message.member.voice.channel.id;
+       const crew = initCrew(message);
+       const traitor = crew[getRandomInt(crew.length)];
+       const mission = getRandomTask(treachery.scenarios);
 
-     const orders = getRandomTask(treachery.scenarios);
-     traitor.send(`Commence operation: ${orders.name}`);
-     traitor.send(`Your mission: ${orders.task}`);
-     console.log(traitor.displayName);
+       let session = new Session(crew, traitor, mission);
+       allSessions.set(chanID, session);
+
+       message.reply('There\'s a traitor in your midst...');
+       traitor.send("You are the traitor.");
+       traitor.send(`Commence operation: ${mission.name}`);
+       traitor.send(`Your mission: ${mission.task}`);
+     } catch (err) {
+       message.channel.send("`Error: Can't do this in a DM channel.`");
+       console.log(err);S
+     }
    }
 
    //Set a specific player as the traitor
    if (message.content.startsWith("!settraitor")) {
-     const member = message.mentions.members.first();
-     if (!member) {
-      return message.reply(
-        'Specified user is not a member of the server.  Syntax:\n' +
-        '`!settraitor @<userName>`'
-      );
-    }
-    //Ensure traitor is not a bot
-    if (member.user.bot) {
-      message.channel.send('"Hello, me...  Meet the *real* me!"');
-      message.channel.send('`Hint: Bots cannot be traitors.`');
-      return;
-    }
-     traitor = member;
-     message.channel.send(`Set ${traitor.displayName} as the traitor.`);
-     member.send("You are the traitor.");
-     const orders = getRandomTask(treachery.scenarios);
-     traitor.send(`Commence operation: ${orders.name}`);
-     traitor.send(`Your mission: ${orders.task}`);
+     try {
+       const member = message.mentions.members.first();
+       if (!member) {
+        return message.reply('Specified user is not a member of the server.  Syntax:\n' +
+          '`!settraitor @<userName>`');
+        }
+      if (member.user.bot) {
+        return message.channel.send('"Hello, me...  Meet the *real* me!"\n' +
+        '`Hint: Bots cannot be traitors.`');
+      }
+      if (!message.member.voice.channel) {
+        return message.reply('You aren\'t in any voice channels!' );
+      }
+      const chanID = message.member.voice.channel.id;
+      const traitor = member;
+      const mission = getRandomTask(treachery.scenarios);
+
+      let session = new Session([], traitor, mission);
+      allSessions.set(chanID, session);
+
+      traitor.send("You are the traitor.");
+      traitor.send(`Commence operation: ${mission.name}`);
+      traitor.send(`Your mission: ${mission.task}`);
+    } catch (err) {
+       message.channel.send("`Error: Can't do this in a DM channel.`");
+       console.log(err);
+      }
    }
 
    //Reveal who the traitor was
    if (message.content === '!reveal') {
-     //Unless traitor is not set, traitor should be an Object and not a String
-     if (typeof traitor === 'string') {
-       message.channel.send('Nobody is the traitor...\n\nYet...');
-       return;
-     }
-     message.channel.send(`The traitor was ${traitor.displayName}!`);
+     try {
+       if (!message.member.voice.channel) {
+         return message.reply('You aren\'t in any voice channels!' );
+       }
+       let session = allSessions.get(message.member.voice.channel.id);
+
+       if (typeof(session) === 'undefined') {
+         return message.channel.send('Nobody is the traitor...\n\nYet...');
+       }
+       message.channel.send(`The traitor was ${session.traitor.displayName}!`);
+       message.channel.send(`Their task was: ${session.mission.task}`);
+    } catch (err) {
+      message.channel.send("`Error: Can't do this in a DM channel.`");
+      console.log(err);
+    }
    }
 
    //Clear all traitors
    if (message.content === '!clear') {
-     traitor = "Nobody";
-     message.channel.send('Traitors have been cleared.');
-   }
-
-    //Initialize traitor array
-   if (message.content === '!init'){
-      if (!message.member.voice.channel) {
-        return message.reply('You aren\'t in any voice channels!' )
-      }
-      init(message);
+     try {
+       if (!message.member.voice.channel) {
+         return message.reply('You aren\'t in any voice channels!' );
+       }
+       allSessions.delete(message.member.voice.channel.id);
+       message.channel.send('Traitors have been cleared.');
+     } catch (err) {
+      message.channel.send("`Error: Can't do this in a DM channel.`");
+      console.log(err);
     }
+   }
 
    // Displays the manual
    if (message.content === '!help'||message.content === '!rtfm') {
@@ -100,32 +125,33 @@ client.on('ready', () => {
 
    //Dump variables to console.  Deprecate after testing.
    if (message.content === '!dump') {
-     console.log('Is Traitor set?');
-     console.log(traitorSet);
-     console.log('Should be value of traitor:');
-     console.log(traitor);
      console.log('Current Channel members:');
-     console.log(init(message));
+     console.log(initCrew(message));
      console.log('Current scenarios:');
      for (x in treachery.scenarios) {
        var t = treachery.scenarios[x].name;
        console.log(t);
      }
-     console.log('A randomly selected task:');
-     var task = getRandomTask(treachery.scenarios);
-     console.log(task);
     }
  });
 
+ class Session {
+   constructor (crew, traitor, mission){
+     this.crew = crew;
+     this.traitor = traitor;
+     this.mission = mission;
+   }
+ }
+
 //Generate an integer between 0 and max, exclusive of max
- function getRandomInt(max) {
+function getRandomInt(max) {
    return Math.floor(Math.random() * Math.floor(max));
  }
 
  //Returns an array of voice channel members
-function init(message){
-  var chanUsers = [];
-  const vChan = message.member.voice.channel;
+function initCrew(message){
+  let chanUsers = [];
+  let vChan = message.member.voice.channel;
   for (const [memberID, member] of vChan.members) {
     message.channel.send(`Adding ${member.displayName}`);
     chanUsers.push(member);
